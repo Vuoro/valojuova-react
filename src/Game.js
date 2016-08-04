fa3681f1e1fc85a4aef6d77c31fb365a0184c398
Joni Korpi <me@jonikorpi.com>
2016-08-04 17:23:55 +0300

    Base commit

---
+++ src/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+  render() {
+    return (
+      <h1>
+        Valojuova!
+      </h1>
+    );
+  }
+}
+
+export default Game;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./Game";
+import "./reset.css";
+import "./main.css";
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById("game")
+);
